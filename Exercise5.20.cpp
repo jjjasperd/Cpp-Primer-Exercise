2ac04663087090e388980b4e52aa3a413053a52c
jjjasperd <jj1991@qq.com>
2015-01-11 22:52:34 -0500

    Exercise5.20
    after input press ctrl+z(win) or command+d(mac/linux) to run.

---
+++ Exercise5.20.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Statement
+//
+//  Created by DuanYujia on 1/9/15.
+//  Copyright (c) 2015 DuanYujia. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main(int argc, const char * argv[]) {
+    string str1,str2;
+    while(cin >> str1){
+        if (str1 ==str2) {
+            cout << endl << "the " << str1 <<" occurs twice." << endl;
+            break;
+        }
+        else
+            str2 = str1;
+        if(cin.eof())
+            cout<< endl << "there is no successive  word being repeated. " << endl;
+    }
+
+    return 0;
+}
