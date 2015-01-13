54e7ee52ca0556ee1bb817330d423ecaf81c431b
jjjasperd <jj1991@qq.com>
2015-01-12 21:24:41 -0500

    Exercise6.12

---
+++ Exercise6.12.cpp
@@ -0,0 +1,26 @@
+//
+//  Exercise6.12.cpp
+//  Function
+//
+//  Created by DuanYujia on 1/12/15.
+//  Copyright (c) 2015 DuanYujia. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+//using reference is easier, no matter from the programming or the storage using
+void swap(int &i, int &j){
+    int sp;
+    sp = i;
+    i = j;
+    j = sp;
+}
+
+int main(int argc, const char * argv[]) {
+    int a = 2;
+    int b = 5;
+    cout<<" a=" << a << ", b=" <<b <<endl;
+    swap(a,b);
+    cout<<" a=" << a << ", b=" <<b <<endl;
+    return 0;
+}
