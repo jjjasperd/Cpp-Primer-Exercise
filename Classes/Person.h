7ef2588f392ebc1ba662b8f2c2078772e88283c9
jjjasperd <jj1991@qq.com>
2015-01-16 16:24:24 -0500

    Exercise 7.9

--- Classes/Person.h
+++ Classes/Person.h
@@ -9,11 +9,22 @@
 #ifndef Classes_Header_h
 #define Classes_Header_h
 #include <string>
+#include <iostream>
 
 struct Person{
-    std::string get_name const { return name; }
-    std::string get_address const { return address;}
+    std::string get_name() const { return name; }
+    std::string get_address() const { return address;}
     std::string name;
     std::string address;
 };
+
+istream &read(istream &is, Person &per){
+    is >> name >> address;
+    return is;
+}
+
+ostream &print(ostream &os, const Person &per){
+    os << per.get_name() <<" "<<per.get_address;
+    return os;
+}
 #endif
