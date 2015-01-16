d389347c6d776ea0950886999f2a73fdaa77ada0
jjjasperd <jj1991@qq.com>
2015-01-16 12:34:15 -0500

    Exercise7.2

--- Classes/Sales_Data.h
+++ Classes/Sales_Data.h
@@ -12,10 +12,19 @@
 
 
 struct Sales_Data{
+    std::string isbn() const { return bookNo; }
+    Sales_Data& combine(const Sales_Data&);
+    double avg_price() const;
+    
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
 
+Sales_Data& Sales_Data::combine(const Sales_Data &rhs){
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
 
 #endif
